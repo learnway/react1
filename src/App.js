8e5036ec5c5c7e49701eebfd05c0e73943dbe09a
learnway <learnfomr@gmail.com>
2025-01-03 19:52:06 +0000

    Add Tailwind css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="App-header">Hello, World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <p></p>
     </>
   );
 }
